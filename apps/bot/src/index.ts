48c16c101fff2511933740e0b5917ecb8ff96859
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2024-10-18 15:02:51 -0300

    feat: add bot initial functionalities

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello, world!')
+import qrcode from 'qrcode-terminal'
+import { Client, LocalAuth } from 'whatsapp-web.js'
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+})
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true })
+})
+
+client.on('ready', () => {
+  console.log('Client is ready!')
+})
+
+client.on('message_create', async (message) => {
+  console.log(message.body)
+  if (message.body === 'ping') {
+    await client.sendMessage(message.from, 'pong')
+  }
+})
+
+client.initialize()
